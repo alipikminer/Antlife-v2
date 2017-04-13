b0f6fa227b80f4ad43615103bdd034f564454fed
alipikminer <alipikminer@yandex.ru>
2017-04-13 18:20:14 +0300

    Муравьи_1
    воткнуты муравьи

--- Program2.pas
+++ Program2.pas
@@ -5,11 +5,31 @@ var
   map: array[1..500] of array[1..500] of array[1..2] of integer;
 //Двуслойный массив карты. Работу осуществлять ТОЛЬКО во втором слоe(map[x][y][2])
 var
-  i, ii: integer;
+  antx, anty, anttime: array of integer;
+
+var
+  antcount: integer;
+
+var
+  i, ii, x, y: integer;
 //переменные для непосредственного обновления экрана
 begin
   randomize();
+  antcount := 2500;
+  
+  SetLength(antx, antcount);
+  SetLength(anty, antcount);
+  SetLength(anttime, antcount);
   //Заполение всего экрана замлёй
+  for i:=1 to antcount-1 do
+  begin
+  x := random(1, 500);
+  y := random(1, 500);
+  antx[i] := x;
+  anty[i] := y;
+  map[antx[i]][anty[i]][2] := 1;
+  anttime[i] := random(250, 1000);
+  end;
   for i := 1 to 500 do
   begin
     for ii := 1 to 500 do
@@ -19,6 +39,19 @@ begin
   end;
   repeat
     //обновление экрана
+    
+    for i := 1 to antcount-1 do
+    begin
+      x:=random(-1,1);
+      y:=random(-1,1);
+      if antx[i] <= 5 then if x <= 0 then x := 1;
+        if anty[i] <= 5 then if y <= 0 then y := 1;
+        if antx[i] >= 495 then if x >= 0 then x := -1;
+        if anty[i] >= 495 then if y >= 0 then y := -1;
+        map[antx[i]][anty[i]][2]:=2;
+      antx[i] := antx[i] + x;    
+      anty[i] := anty[i] + y;
+    end;
     for i := 1 to 500 do
     begin
       for ii := 1 to 500 do
@@ -37,5 +70,9 @@ begin
         map[i][ii][1] := map[i][ii][2];
       end;
     end;
+    
+    SetLength(antx, antcount);
+    SetLength(anty, antcount);
+  SetLength(anttime, antcount);
   until 2 = 3;
 end.
